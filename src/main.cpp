043e43bb89277089b900ad8af61d0000c3d7136b
Lucas Lin <lucaslin989@gamil.com>
2026-02-17 21:55:34 -0700

    Done, half

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Vector2D{
+    public:
+        double x;
+        double y;
+        Vector2D(double x=0, double y=0):x(x), y(y){}
+
+        Vector2D add(const Vector2D&other)const{
+            return{x+other.x,y+other.y};
+        }
+
+};
+
+
+int main(){
+    Vector2D position(10,5);
+    Vector2D velocity(1,-2);
+    position=position.add(velocity);
+
+    cout<<"Position: ( "<<position.x<<", "<<position.y<<")\n";
+
+    return 0;
+}
